e248219d0e995717e25e655437e6ea3df7564391
Edouard Shaar <Zarthia@gmail.com>
2014-04-23 09:08:57 -0400

    Taxonomy Session manager is now no longer a singleton. We where having problems where the term store cache we not refreshed after manipulations.

--- Source/GSoft.Dynamite.DI.Unity/UnityDynamiteRegistrationModule.cs
+++ Source/GSoft.Dynamite.DI.Unity/UnityDynamiteRegistrationModule.cs
@@ -121,7 +121,7 @@ namespace GSoft.Dynamite.DI.Unity
             container.RegisterType<PageCreator>();
 
             // Taxonomy
-            container.RegisterType<ITaxonomySessionManager, TaxonomySessionManager>(new ContainerControlledLifetimeManager());
+            container.RegisterType<ITaxonomySessionManager, TaxonomySessionManager>();
             container.RegisterType<ITaxonomyService, TaxonomyService>();
             container.RegisterType<TaxonomyService>();
             container.RegisterType<TaxonomyHelper>();
