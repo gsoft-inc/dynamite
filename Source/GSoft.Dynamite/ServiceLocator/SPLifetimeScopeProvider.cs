2248ab7e0a1d058fa8c31c3e0150b1a4124af694
Etienne Sauvé <etienne.sauve@gmail.com>
2014-05-02 15:21:28 -0400

    Removed nuget packages.config, added fastjson to binaries, added some docs

---
+++ Source/GSoft.Dynamite/ServiceLocator/SPLifetimeScopeProvider.cs
@@ -0,0 +1,103 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="LifetimeScopeProvider.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace GSoft.Dynamite.ServiceLocator
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    using Autofac;
+
+    using Microsoft.SharePoint;
+
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    public class SPLifetimeScopeProvider : IDisposable
+    {
+        private readonly IContainer rootContainer;
+
+        private Dictionary<object, ILifetimeScope> lifetimeScopes;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SPLifetimeScopeProvider"/> class.
+        /// </summary>
+        /// <param name="rootContainer">
+        /// The root Container.
+        /// </param>
+        public SPLifetimeScopeProvider(IContainer rootContainer)
+        {
+            this.rootContainer = rootContainer;
+            this.lifetimeScopes = new Dictionary<object, ILifetimeScope>();
+        }
+
+        public IContainer Current
+        {
+            get
+            {
+                return this.rootContainer;
+            }
+        }
+
+        /// <summary>
+        /// Gets the web application.
+        /// </summary>
+        public ILifetimeScope WebApplication
+        {
+            get
+            {
+                return this.Current.BeginLifetimeScope(SPContext.Current.Site.WebApplication);
+            }
+        }
+
+        /// <summary>
+        /// Gets the site.
+        /// </summary>
+        public ILifetimeScope Site
+        {
+            get
+            {
+                return this.Current.BeginLifetimeScope(SPContext.Current.Site.ID);
+            }
+        }
+
+        /// <summary>
+        /// Gets the web.
+        /// </summary>
+        public ILifetimeScope Web
+        {
+            get
+            {
+                return this.Current.BeginLifetimeScope(SPContext.Current.Web.ID);
+            }
+        }
+
+        /// <summary>
+        /// The externally controlled scope.
+        /// </summary>
+        /// <param name="key">
+        /// The key.
+        /// </param>
+        /// <returns>
+        /// The <see cref="ILifetimeScope"/>.
+        /// </returns>
+        public ILifetimeScope ExternallyControlledScope(object key)
+        {
+            return this.Current.BeginLifetimeScope(key);
+        }
+
+        public void Dispose()
+        {
+            // Dispose all lifetimescopes
+            foreach (var lifetimeScope in this.lifetimeScopes.Values)
+            {
+                lifetimeScope.Dispose();
+            }
+        }
+    }
+}
--- Source/GSoft.Dynamite/Taxonomy/SiteTaxonomyCache.cs
+++ Source/GSoft.Dynamite/Taxonomy/SiteTaxonomyCache.cs
@@ -1,15 +1,34 @@
 ﻿using System;
-using Microsoft.SharePoint.Taxonomy;
+
 using Microsoft.SharePoint;
+using Microsoft.SharePoint.Taxonomy;
 
 namespace GSoft.Dynamite.Taxonomy
 {
+    /// <summary>
+    /// The site taxonomy cache.
+    /// </summary>
     public class SiteTaxonomyCache
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SiteTaxonomyCache"/> class.
+        /// </summary>
+        /// <param name="site">
+        /// The site.
+        /// </param>
         public SiteTaxonomyCache(SPSite site) : this(site, null)
         {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SiteTaxonomyCache"/> class.
+        /// </summary>
+        /// <param name="site">
+        /// The site.
+        /// </param>
+        /// <param name="termStoreName">
+        /// The term store name.
+        /// </param>
         public SiteTaxonomyCache(SPSite site, string termStoreName)
         {
             this.SiteId = site.ID;
@@ -28,10 +47,19 @@ namespace GSoft.Dynamite.Taxonomy
             }
         }
 
+        /// <summary>
+        /// Gets or sets the site id.
+        /// </summary>
         public Guid SiteId { get; set; }
 
+        /// <summary>
+        /// Gets or sets the taxonomy session.
+        /// </summary>
         public TaxonomySession TaxonomySession { get; set; }
 
+        /// <summary>
+        /// Gets or sets the site collection group.
+        /// </summary>
         public Group SiteCollectionGroup { get; set; }
     }
 }
