1e53f06d310aa74cf85f7d9814069833270944c0
Thomas-Antoine O'Neill <thomasantoine.oneill@gmail.com>
2014-05-20 10:54:58 -0400

    [ServiceLocator] - Added BeginWebLifetimeScope to ISharePointServiceLocator interface, replicating what we did in Dynamite-2013 project to support using container from C# Cmdlets in Powershell.DLL project - Merging this back from upstream would've sucked too much

--- Source/GSoft.Dynamite/ServiceLocator/AddOnProvidedServiceLocator.cs
+++ Source/GSoft.Dynamite/ServiceLocator/AddOnProvidedServiceLocator.cs
@@ -101,6 +101,36 @@ namespace GSoft.Dynamite.ServiceLocator
         }
 
         /// <summary>
+        /// Creates a new child lifetime scope under the scope of the specified web
+        /// (allowing you to inject InstancePerSite and InstancePerWeb objects).
+        /// Please dispose this lifetime scope when done (E.G. call this method from
+        /// a using block).
+        /// Prefer usage of this method versus resolving manually from the Current property.
+        /// </summary>
+        /// <param name="feature">The current web from which we are requesting a child lifetime scope</param>
+        /// <returns>A new child lifetime scope which should be disposed by the caller.</returns>
+        public ILifetimeScope BeginWebLifetimeScope(SPWeb web)
+        {
+            ILifetimeScope newWebChildScope = null;
+
+            if (web == null)
+            {
+                // Can't use an AddOnProvidedServiceLocator this way outside a SPSite context (e.g. no SPFarm or SPWebApp scoped feature will work)
+                throw new ArgumentNullException("web");
+            }
+            else
+            {
+                this.EnsureServiceLocatorAccessorForCurrentSiteContext(web.Site);
+
+                // We are dealing with a SPSite or SPWeb-scoped feature.
+                // Always return a web scope (even for Site-scoped features - as being in a site-scoped feature means you are in the RootWeb context)
+                newWebChildScope = this.locatorAccessor.ServiceLocatorInstance.BeginWebLifetimeScope(web);
+            }
+
+            return newWebChildScope;
+        }
+
+        /// <summary>
         /// Autowires the dependencies of a UI control using the current HTTP-request-bound
         /// lifetime scope.
         /// Prefer usage of this method versus resolving indididual dependencies from the 
