3a7f1b8b15e8d62c41d41e49037eea377430d1a8
Thomas-Antoine O'Neill <thomasantoine.oneill@gmail.com>
2014-06-20 12:16:26 -0400

    [Cache] - Cache key group discriminator couldn't support AD groups assigned to SharePoint groups (i.e. we could only obtain a list of a user's group if that user was directly assigned to a SharePoint group) - Now the SimpleCacheKey maintains a static dictionary of all user's cache prefix that allows them to share the same cache as others in the same group - So now AD users have a proper group discriminator as part of the cache keys they use (no more "NoGroup" discriminator for everyone, which was causing bad permission and GetById issues)

--- Source/GSoft.Dynamite/Cache/SimpleCacheKey.cs
+++ Source/GSoft.Dynamite/Cache/SimpleCacheKey.cs
@@ -1,5 +1,7 @@
 ﻿using System.Linq;
 using Microsoft.SharePoint;
+using System.Collections.Generic;
+using System.Web;
 
 namespace GSoft.Dynamite.Cache
 {
@@ -8,6 +10,9 @@ namespace GSoft.Dynamite.Cache
     /// </summary>
     public class SimpleCacheKey : ICacheKey
     {
+        // keep one cached discriminator per user, per site (not threadsafe, but no worries)
+        private static IDictionary<string, string> userSiteGroupDiscriminators = new Dictionary<string, string>();
+
         /// <summary>
         /// The prefix of the keep to identify the Dynamite cache keys in the HttpCache.
         /// </summary>
@@ -53,20 +58,43 @@ namespace GSoft.Dynamite.Cache
                 && SPContext.Current.Web.CurrentUser != null)
             {
                 // We have an authenticated user
-                if (SPContext.Current.Web.CurrentUser.Groups != null
-                    && SPContext.Current.Web.CurrentUser.Groups.Count > 0)
+                var currentWeb = SPContext.Current.Web;
+                var currentUser = currentWeb.CurrentUser;
+
+                var discriminatorCacheKey = currentWeb.Site.Url + "_" + currentUser.LoginName;
+
+                if (userSiteGroupDiscriminators.ContainsKey(discriminatorCacheKey))
                 {
-                    // Add the ordered list of all group names as second prefix to the cache key
-                    var groupNamesArray = SPContext.Current.Web.CurrentUser.Groups
-                        .Cast<SPGroup>()
-                        .OrderBy(group => group.Name)
-                        .Select(group => group.Name)
-                        .ToArray();
-                    discriminator = string.Join("_", groupNamesArray);
+                    discriminator = userSiteGroupDiscriminators[discriminatorCacheKey];
                 }
                 else
                 {
-                    discriminator = "NoGroup";
+                    // no cached discriminator for that site+user combination yet
+                    var currentUserGroups = new List<SPGroup>();
+
+                    foreach (SPGroup siteGroup in currentWeb.SiteGroups)
+                    {
+                        if (siteGroup.ContainsCurrentUser)
+                        {
+                            currentUserGroups.Add(siteGroup);
+                        }
+                    }
+
+                    if (currentUserGroups.Count > 0)
+                    {
+                        // Add the ordered list of all group names as second prefix to the cache key
+                        var groupNamesArray = currentUserGroups.OrderBy(group => group.Name)
+                            .Select(group => group.Name).ToArray();
+                        discriminator = string.Join("_", groupNamesArray);
+                    }
+                    else
+                    {
+                        discriminator = "NoGroup";
+                    }
+
+                    // cache the discriminator value for later (don't worry too much about multithreaded
+                    // access - at worse we evaluate the discriminator a couple of times in a row)
+                    userSiteGroupDiscriminators[discriminatorCacheKey] = discriminator;
                 }
             }
             else
