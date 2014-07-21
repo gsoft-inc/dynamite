cf96e958e5be8d886bfad0e182b1e1f6c9984746
Thomas-Antoine O'Neill <thomasantoine.oneill@gmail.com>
2014-07-21 16:12:33 -0400

    [AssemblyInfo] - Remove CommonAssemblyInfo references because the TeamCity AssemblyPatch feature won't work with it - New convention: all Dynamite 2010 packages should be versionned v1.0.0.0 to simplify patching (so that all users of the DLL don't need to be rebuilt everytime Dynamite changes). Updates should be tracked through the NuGet package number and the assemblyfileversion.

--- Source/GSoft.Dynamite/Properties/AssemblyInfo.cs
+++ Source/GSoft.Dynamite/Properties/AssemblyInfo.cs
@@ -20,3 +20,13 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("d377829a-3b91-4ac9-8569-694ee8f48fb7")]
+
+// Keep AssemblyVersion info in each project so that the TeamCity assembly version patcher works properly
+// A DLL used as third-party reference in a SharePoint context is easier to patch if its AssemblyVersion
+// never changes (otherwise, runtime assembly resolution will break every time you redeploy and newly
+// version-bumped assembly of the third-party).
+[assembly: AssemblyVersion("1.0.0.0")]
+
+// The AssemblyFileVersion is used to track the current version.
+// The version 0.0.0.0 flags a developement machine-build artifact.
+[assembly: AssemblyFileVersion("0.0.0.0")]
