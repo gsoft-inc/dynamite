0f634bd27193d88d2bbdebb6841189c2797754a9
Etienne Sauvé <etienne.sauve@gmail.com>
2014-10-03 17:18:33 -0400

    Added CAML.Net to references, needed for ItemLocator. Registered the new IItemLocator implementation

---
+++ Source/GSoft.Dynamite/Repositories/ItemLocator.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GSoft.Dynamite.Repositories
+{
+    using GSoft.Dynamite.Lists;
+
+    using JohnHolliday.Caml.Net;
+
+    using Microsoft.SharePoint;
+
+    /// <summary>
+    /// The item locator.
+    /// </summary>
+    public class ItemLocator : IItemLocator
+    {
+        private readonly ListLocator listLocator;
+
+        /// <summary>
+        /// Utility to find items by name or url
+        /// </summary>
+        /// <param name="listLocator">List locator utility</param>
+        public ItemLocator(ListLocator listLocator)
+        {
+            this.listLocator = listLocator;
+        }
+
+        /// <summary>
+        /// Get the list item corresponding to the given title 
+        /// </summary>
+        /// <param name="web">The web.</param>
+        /// <param name="listUrl">The display name to reach the list.</param>
+        /// <param name="itemTitle">The title of the list item.</param>
+        /// <returns>
+        /// The <see cref="SPSecurableObject"/>.
+        /// </returns>
+        public SPSecurableObject GetByTitle(SPWeb web, string listUrl, string itemTitle)
+        {
+            SPList list = this.listLocator.GetByUrl(web, listUrl);
+
+            SPQuery query = new SPQuery();
+
+            query.Query =
+                CAML.Where(
+                    CAML.Or(
+                        CAML.Eq(CAML.FieldRef(BuiltInFields.TitleName), CAML.Value(itemTitle)),
+                        CAML.Eq(CAML.FieldRef(BuiltInFields.FileLeafRefName), CAML.Value(itemTitle))));
+
+            var itemCollection = list.GetItems(query);
+
+            return itemCollection.Count > 0 ? itemCollection[0] : null;
+        }
+    }
+}
