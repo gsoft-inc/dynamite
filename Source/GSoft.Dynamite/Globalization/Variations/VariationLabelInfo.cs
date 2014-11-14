a05bb249ee27b75361da46dba748f0a77fb6d4f2
Franck Cornu <franck.cornu@gsoft-group.com>
2014-11-14 16:17:20 -0500

    - Correction d'un bug dans la construction d'un VariationLabelInfo. Le calcul de la Locale cause une exception car la valeur de variation.Locale est déjà un LCID.

--- Source/GSoft.Dynamite/Globalization/Variations/VariationLabelInfo.cs
+++ Source/GSoft.Dynamite/Globalization/Variations/VariationLabelInfo.cs
@@ -37,7 +37,7 @@ namespace GSoft.Dynamite.Globalization.Variations
             this.FlagControlDisplayName = variationLabel.DisplayName;
             this.IsSource = variationLabel.IsSource;
             this.Language = variationLabel.Language;
-            this.Locale = new CultureInfo(variationLabel.Locale).LCID;
+            this.Locale = new CultureInfo(variationLabel.Language).LCID;
             this.Title = variationLabel.Title;
             this.TopWebUrl = new Uri(variationLabel.TopWebUrl);
         }
