3b40aeb1f007184ab7946b20f19f803e8cfaf3a9
unknown <philippe.lavoie@VMSPPHIL.DEV.OFFICE.GSOFT-GROUP.COM>
2014-11-18 16:25:22 -0500

    [File Helper] Add the Files namespace, a FileInfo object and the FileHelper to create file in list

---
+++ Source/GSoft.Dynamite/Files/FileInfo.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GSoft.Dynamite.Files
+{
+    /// <summary>
+    /// Object containing information about a file. Used to create a file in a list/library
+    /// </summary>
+    public class FileInfo
+    {
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        /// <param name="filename">The filename</param>
+        /// <param name="data">The data stream</param>
+        /// <param name="overwrite">If we overwrite the file</param>
+        public FileInfo(string filename, Stream data, bool overwrite)
+        {
+            this.Filename = filename;
+            this.Data = data;
+            this.Overwrite = overwrite;
+        }
+
+        /// <summary>
+        /// Constructor with overwrite at false
+        /// </summary>
+        /// <param name="filename">The filename</param>
+        /// <param name="data">The file data</param>
+        public FileInfo(string filename, Stream data)
+            : this(filename, data, false)
+        {
+        }
+
+        /// <summary>
+        /// The filename of the file. usually contain the extension
+        /// </summary>
+        public string Filename { get; set; }
+
+        /// <summary>
+        /// The data stream to write in the file
+        /// </summary>
+        public Stream Data { get; set; }
+
+        /// <summary>
+        /// If we overwite the file or not
+        /// </summary>
+        public bool Overwrite { get; set; }
+
+        /// <summary>
+        /// The Url of the file, to check if exist
+        /// </summary>
+        public string Url { get; set; }
+    }
+}
