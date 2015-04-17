bd6131739d8325fc7e4999d737a0f408c182c74a
Franck Cornu <franck.cornu@gsoft-group.com>
2015-04-17 17:45:17 -0400

    [DSP][Excel] Correction d'un bug qui ne lisait plus les valeurs d'un fichier Excel un fois modifié par l'utilisateur dans l'application. [DSP][Excel] Ajout de la cmdlet Find-DSPExcelFiles permettant de rechercher dans un ensemble de fichiers Excel d'un répertoire

--- Source/GSoft.Dynamite.PowerShell/Dynamite.PowerShell.Toolkit/Dynamite.PowerShell.Toolkit.psm1
+++ Source/GSoft.Dynamite.PowerShell/Dynamite.PowerShell.Toolkit/Dynamite.PowerShell.Toolkit.psm1
@@ -126,7 +126,7 @@ Export-ModuleMember New-DSPWebXml, Set-DSPWebSearchSettings, Export-DSPWebStruct
 Export-ModuleMember Set-DSPUserProfileSchema
 
 # Excel
-Export-ModuleMember Merge-DSPExcelColumns, Remove-DSPExcelColumn, Copy-DSPExcelColumn , Add-DSPExcelColumn , Edit-DSPExcelColumnValue, Get-DSPExcelFileContent, Open-DSPExcelFile
+Export-ModuleMember Merge-DSPExcelColumns, Remove-DSPExcelColumn, Copy-DSPExcelColumn , Add-DSPExcelColumn , Edit-DSPExcelColumnValue, Get-DSPExcelFileContent, Open-DSPExcelFile, Find-DSPExcelFiles
 
 # Faceted Search
 Export-ModuleMember New-DSPFacetedSearchSettings
--- Source/GSoft.Dynamite.Powershell.Tests/Integration/Excel/Excel.Tests.template.ps1
+++ Source/GSoft.Dynamite.Powershell.Tests/Integration/Excel/Excel.Tests.template.ps1
@@ -9,6 +9,7 @@ $sutPath = "$here\..\..\GSoft.Dynamite.PowerShell\$sut"
 # ----------------------
 
 $ExcelValidFilePath = Join-Path -Path "$here" -ChildPath ".\ExportSharegate_Valid.xlsx"
+$ExcelValidFilePathUserModified = Join-Path -Path "$here" -ChildPath ".\ExportSharegate_UserModified.xlsx"
 
 Describe "Open-DSPExcelFile" {
 
@@ -43,7 +44,7 @@ Describe "Get-DSPExcelFileContent" {
 			{ $ExcelFile | Get-DSPExcelFileContent -Columns @("Column1","Column2","Column3") -WorksheetName "FakeWorksheet" } | Should Throw
 						
 			# Test teardown
-			Remove-Item $TempFolder -Recurse -Confirm:$false
+			Remove-Item $TempFolder -Recurse -Confirm:$false -Force
 		}
 		
 		It "should dispose the Excel file object after execution if '-NoDispose' parameter isn't present" {
@@ -59,7 +60,7 @@ Describe "Get-DSPExcelFileContent" {
 			{ $ExcelFile | Get-DSPExcelFileContent -Columns @("Column1","Column2","Column3") } | Should Throw
 			
 			# Test teardown
-			Remove-Item $TempFolder -Recurse -Confirm:$false
+			Remove-Item $TempFolder -Recurse -Confirm:$false -Force
 		}
 
 		It "should get the content of each row for specified columns" {
@@ -82,6 +83,27 @@ Describe "Get-DSPExcelFileContent" {
 				$FileContent[2].Column2 | Should Be "Value222"
 				$FileContent[2].Column3 | Should Be "Value333"									
 			}
+
+		It "should get the content of the file even if it was modified by user in Excel" {
+		
+				$ExcelFile = Open-DSPExcelFile -Path $ExcelValidFilePathUserModified
+							
+				$FileContent =  $ExcelFile | Get-DSPExcelFileContent -Columns @("Column1","Column2","Column3")
+				
+				# Test values
+				$FileContent.Count | Should Be 3
+				$FileContent[0].Column1 | Should Be "Value1"
+				$FileContent[0].Column2 | Should Be "Value2"
+				$FileContent[0].Column3 | Should Be "Value3"
+				
+				$FileContent[1].Column1 | Should Be "Value11"
+				$FileContent[1].Column2 | Should Be "Value22"
+				$FileContent[1].Column3 | Should Be "Value33"
+				
+				$FileContent[2].Column1 | Should Be "Value111"
+				$FileContent[2].Column2 | Should Be "Value222"
+				$FileContent[2].Column3 | Should Be "Value333"	
+		}
 	}
 }
 
@@ -102,7 +124,7 @@ Describe "Merge-DSPExcelColumns" {
 			{ $ExcelFile | Merge-DSPExcelColumns -TargetColumn "Column1" -SourceColumns @("Column2","Column3") -WorksheetName "FakeWorksheet" } | Should Throw
 			
 			# Test teardown
-			Remove-Item $TempFolder -Recurse -Confirm:$false
+			Remove-Item $TempFolder -Recurse -Confirm:$false -Force
 		}
 		
 		It "should dispose the Excel file object after execution if '-NoDispose' parameter isn't present" {
@@ -118,7 +140,7 @@ Describe "Merge-DSPExcelColumns" {
 			{ $ExcelFile | Merge-DSPExcelColumns -TargetColumn "Column1" -SourceColumns @("Column2","Column3") } | Should Throw
 			
 			# Test teardown
-			Remove-Item $TempFolder -Recurse -Confirm:$false
+			Remove-Item $TempFolder -Recurse -Confirm:$false -Force
 		}
 
 		It "should merge the content of the specified columns into the target columns" {
@@ -138,8 +160,28 @@ Describe "Merge-DSPExcelColumns" {
 			$FileContent[2].Column1 | Should Be "Value111Value222Value333"
 			
 			# Test teardown
-			Remove-Item $TempFolder -Recurse -Confirm:$false
-		}		
+			Remove-Item $TempFolder -Recurse -Confirm:$false -Force
+		}	
+	
+		It "should merge the content of the specified columns into the target columns even if it was modified by user in Excel" {
+		
+			# Create a copy of the file
+			$TempFolder = New-Item -ItemType 'Directory' -Path $here -Name "Temp" -Force
+			$CopiedItem = Copy-Item -Path $ExcelValidFilePathUserModified -Destination $TempFolder -PassThru -Force
+			$ExcelFile = Open-DSPExcelFile -Path $CopiedItem.FullName
+
+			$ExcelFile | Merge-DSPExcelColumns -TargetColumn "Column1" -SourceColumns @("Column2","Column3") -NoDispose
+
+			$FileContent =  $ExcelFile | Get-DSPExcelFileContent -Columns @("Column1")
+
+			# Test values
+			$FileContent[0].Column1 | Should Be "Value1Value2Value3"
+			$FileContent[1].Column1 | Should Be "Value11Value22Value33"
+			$FileContent[2].Column1 | Should Be "Value111Value222Value333"
+			
+			# Test teardown
+			Remove-Item $TempFolder -Recurse -Confirm:$false -Force	
+		}	
 	}
 }
 
@@ -160,7 +202,7 @@ Describe "Add-DSPExcelColumn" {
 			{ $ExcelFile | Add-DSPExcelColumn -ColumnName "NewColumn" -WorksheetName "FakeWorksheet" } | Should Throw
 			
 			# Test teardown
-			Remove-Item $TempFolder -Recurse -Confirm:$false
+			Remove-Item $TempFolder -Recurse -Confirm:$false -Force
 		}
 		
 		It "should dispose the Excel file object after execution if '-NoDispose' parameter isn't present" {
@@ -176,7 +218,7 @@ Describe "Add-DSPExcelColumn" {
 			{ $ExcelFile | Add-DSPExcelColumn -ColumnName "NewColumn" } | Should Throw
 			
 			# Test teardown
-			Remove-Item $TempFolder -Recurse -Confirm:$false
+			Remove-Item $TempFolder -Recurse -Confirm:$false -Force
 		}
 
 		It "should add the column in the file" {
@@ -194,7 +236,7 @@ Describe "Add-DSPExcelColumn" {
 			$FileContent[0].NewColumn | Should Not Be Null
 
 			# Test teardown
-			Remove-Item $TempFolder -Recurse -Confirm:$false
+			Remove-Item $TempFolder -Recurse -Confirm:$false -Force
 		}	
 	
 		It "should add the column in the file and set values as IDs for each row" {
@@ -212,7 +254,7 @@ Describe "Add-DSPExcelColumn" {
 			$FileContent[0].NewColumn | Should Not Be Null
 
 			# Test teardown
-			Remove-Item $TempFolder -Recurse -Confirm:$false
+			Remove-Item $TempFolder -Recurse -Confirm:$false -Force
 		}			
 	}
 }
@@ -234,7 +276,7 @@ Describe "Remove-DSPExcelColumn" {
 			{ $ExcelFile | Remove-DSPExcelColumn -ColumnName "Column1" -WorksheetName "FakeWorksheet" } | Should Throw
 			
 			# Test teardown
-			Remove-Item $TempFolder -Recurse -Confirm:$false
+			Remove-Item $TempFolder -Recurse -Confirm:$false -Force
 		}
 		
 		It "should dispose the Excel file object after execution if '-NoDispose' parameter isn't present" {
@@ -250,7 +292,7 @@ Describe "Remove-DSPExcelColumn" {
 			{ $ExcelFile | Remove-DSPExcelColumn -ColumnName "Column1" } | Should Throw
 			
 			# Test teardown
-			Remove-Item $TempFolder -Recurse -Confirm:$false
+			Remove-Item $TempFolder -Recurse -Confirm:$false -Force
 		}
 
 		It "should remove the column in the file" {
@@ -269,8 +311,27 @@ Describe "Remove-DSPExcelColumn" {
 			$FileContent[0].Column2 | Should Not Be Null
 
 			# Test teardown
-			Remove-Item $TempFolder -Recurse -Confirm:$false
+			Remove-Item $TempFolder -Recurse -Confirm:$false -Force
 		}		
+
+		It "should remove the column in the file even if it was modified by user in Excel" {
+
+			# Create a copy of the file
+			$TempFolder = New-Item -ItemType 'Directory' -Path $here -Name "Temp" -Force
+			$CopiedItem = Copy-Item -Path $ExcelValidFilePathUserModified -Destination $TempFolder -PassThru -Force
+			$ExcelFile = Open-DSPExcelFile -Path $CopiedItem.FullName
+
+			$ExcelFile | Remove-DSPExcelColumn -ColumnName "Column1" -NoDispose
+
+			$FileContent =  $ExcelFile | Get-DSPExcelFileContent -Columns @("Column1","Column2")
+			
+			# Test values
+			$FileContent[0].Column1 | Should Throw
+			$FileContent[0].Column2 | Should Not Be Null
+
+			# Test teardown
+			Remove-Item $TempFolder -Recurse -Confirm:$false -Force
+		}
 	}
 }
 
@@ -291,7 +352,7 @@ Describe "Copy-DSPExcelColumn" {
 			{ $ExcelFile | Copy-DSPExcelColumn -SourceColumn "Column2" -TargetColumn "Column1" -WorksheetName "FakeWorksheet" }  | Should Throw
 			
 			# Test teardown
-			Remove-Item $TempFolder -Recurse -Confirm:$false
+			Remove-Item $TempFolder -Recurse -Confirm:$false -Force
 		}
 		
 		It "should dispose the Excel file object after execution if '-NoDispose' parameter isn't present" {
@@ -307,7 +368,7 @@ Describe "Copy-DSPExcelColumn" {
 			{ $ExcelFile | Copy-DSPExcelColumn -SourceColumn "Column2" -TargetColumn "Column1" }  | Should Throw
 			
 			# Test teardown
-			Remove-Item $TempFolder -Recurse -Confirm:$false
+			Remove-Item $TempFolder -Recurse -Confirm:$false -Force
 		}
 
 		It "should copy the content between a source column and a target column for each row in the file" {
@@ -332,8 +393,33 @@ Describe "Copy-DSPExcelColumn" {
 			$FileContent[2].Column2 | Should Be "Value222"
 
 			# Test teardown
-			Remove-Item $TempFolder -Recurse -Confirm:$false
-		}		
+			Remove-Item $TempFolder -Recurse -Confirm:$false -Force
+		}	
+	
+		It "should copy the content between a source column and a target column for each row in the file even if it was modified by user in Excel" {
+
+			# Create a copy of the file
+			$TempFolder = New-Item -ItemType 'Directory' -Path $here -Name "Temp" -Force
+			$CopiedItem = Copy-Item -Path $ExcelValidFilePathUserModified -Destination $TempFolder -PassThru -Force
+			$ExcelFile = Open-DSPExcelFile -Path $CopiedItem.FullName
+
+			$ExcelFile | Copy-DSPExcelColumn -SourceColumn "Column2" -TargetColumn "Column1" -NoDispose
+
+			$FileContent =  $ExcelFile | Get-DSPExcelFileContent -Columns @("Column1","Column2")
+		
+			# Test values
+			$FileContent[0].Column1 | Should Be "Value2"
+			$FileContent[0].Column2 | Should Be "Value2"
+
+			$FileContent[1].Column1 | Should Be "Value22"
+			$FileContent[1].Column2 | Should Be "Value22"
+
+			$FileContent[2].Column1 | Should Be "Value222"
+			$FileContent[2].Column2 | Should Be "Value222"
+
+			# Test teardown
+			Remove-Item $TempFolder -Recurse -Confirm:$false -Force
+		}	
 	}
 }
 
@@ -354,7 +440,7 @@ Describe "Edit-DSPExcelColumn" {
 			{ $ExcelFile | Edit-DSPExcelColumnValue -Pattern "Value" -Value "Replaced" -WorksheetName "FakeWorksheet" } | Should Throw
 			
 			# Test teardown
-			Remove-Item $TempFolder -Recurse -Confirm:$false
+			Remove-Item $TempFolder -Recurse -Confirm:$false -Force
 		}
 		
 		It "should dispose the Excel file object after execution if '-NoDispose' parameter isn't present" {
@@ -370,7 +456,7 @@ Describe "Edit-DSPExcelColumn" {
 			{ $ExcelFile | Edit-DSPExcelColumnValue -Pattern "Value" -Value "Replaced" } | Should Throw
 			
 			# Test teardown
-			Remove-Item $TempFolder -Recurse -Confirm:$false
+			Remove-Item $TempFolder -Recurse -Confirm:$false -Force
 		}
 
 		It "should replace the value in the whole file if no column is specified" {
@@ -398,7 +484,7 @@ Describe "Edit-DSPExcelColumn" {
 			$FileContent[2].Column3 | Should Be "Replaced333"		
 
 			# Test teardown
-			Remove-Item $TempFolder -Recurse -Confirm:$false
+			Remove-Item $TempFolder -Recurse -Confirm:$false -Force
 		}		
 
 		It "should replace all values matching the regex token" {
@@ -421,7 +507,31 @@ Describe "Edit-DSPExcelColumn" {
 			$FileContent[2].User | Should Be "NEWDOMAIN\john.doe"
 
 			# Test teardown
-			Remove-Item $TempFolder -Recurse -Confirm:$false	
+			Remove-Item $TempFolder -Recurse -Confirm:$false -Force	
+		}
+
+		It "should replace all values matching the regex token even if it was modified by user in Excel" {
+
+			# Create a copy of the file
+			$TempFolder = New-Item -ItemType 'Directory' -Path $here -Name "Temp" -Force
+			$CopiedItem = Copy-Item -Path $ExcelValidFilePathUserModified -Destination $TempFolder -PassThru -Force
+			$ExcelFile = Open-DSPExcelFile -Path $CopiedItem.FullName
+
+			$ExcelFile | Edit-DSPExcelColumnValue -Pattern "DEV\\\w+" -Value "NEWDOMAIN\john.doe" -NoDispose
+
+			$FileContent =  $ExcelFile | Get-DSPExcelFileContent -Columns @("User")
+		
+			# Test values
+			$FileContent.Count | Should Be 3
+			$FileContent[0].User | Should Be "NEWDOMAIN\john.doe"
+				
+			$FileContent[1].User | Should Be "NEWDOMAIN\john.doe"
+	
+			$FileContent[2].User | Should Be "NEWDOMAIN\john.doe"
+
+			# Test teardown
+			Remove-Item $TempFolder -Recurse -Confirm:$false -Force	
+
 		}
 
 		It "should replace values only in the specified column" {
@@ -450,7 +560,36 @@ Describe "Edit-DSPExcelColumn" {
 			$FileContent[2].Column3 | Should Be "Value333"
 
 			# Test teardown
-			Remove-Item $TempFolder -Recurse -Confirm:$false
+			Remove-Item $TempFolder -Recurse -Confirm:$false -Force
+		}
+
+		It "should replace values only in the specified column even if it was modified by user in Excel" {
+
+			# Create a copy of the file
+			$TempFolder = New-Item -ItemType 'Directory' -Path $here -Name "Temp" -Force
+			$CopiedItem = Copy-Item -Path $ExcelValidFilePathUserModified -Destination $TempFolder -PassThru -Force
+			$ExcelFile = Open-DSPExcelFile -Path $CopiedItem.FullName
+
+			$ExcelFile | Edit-DSPExcelColumnValue -Pattern "Value" -Value "Replaced" -Column "Column1" -NoDispose
+
+			$FileContent =  $ExcelFile | Get-DSPExcelFileContent -Columns @("Column1","Column2","Column3")
+		
+			# Test values
+			$FileContent.Count | Should Be 3
+			$FileContent[0].Column1 | Should Be "Replaced1"
+			$FileContent[0].Column2 | Should Be "Value2"
+			$FileContent[0].Column3 | Should Be "Value3"
+				
+			$FileContent[1].Column1 | Should Be "Replaced11"
+			$FileContent[1].Column2 | Should Be "Value22"
+			$FileContent[1].Column3 | Should Be "Value33"
+				
+			$FileContent[2].Column1 | Should Be "Replaced111"
+			$FileContent[2].Column2 | Should Be "Value222"
+			$FileContent[2].Column3 | Should Be "Value333"
+
+			# Test teardown
+			Remove-Item $TempFolder -Recurse -Confirm:$false -Force
 		}
 		
 		It "should generate IDs in the column if the -AsIdentifier parameter is specified" {
@@ -473,7 +612,41 @@ Describe "Edit-DSPExcelColumn" {
 			$FileContent[2].Column1 | Should Be "3"
 
 			# Test teardown
-			Remove-Item $TempFolder -Recurse -Confirm:$false			
+			Remove-Item $TempFolder -Recurse -Confirm:$false -Force			
 		}
 	}
+}
+
+
+Describe "Find-DSPExcelFiles" {
+
+	It "should throw an error if the specified worksheet doesn't exist in the file" {
+			
+			# Create a copy of the file
+			$TempFolder = New-Item -ItemType 'Directory' -Path $here -Name "Temp" -Force
+			$CopiedItem = Copy-Item -Path $ExcelValidFilePath -Destination $TempFolder -PassThru -Force
+
+			{ Find-DSPExcelFiles -Folder $TempFolder.FullName -Columns "Column1" -Patterns "Value" -WorksheetName "FakeWorksheet" } | Should Throw
+			
+			# Test teardown
+			Remove-Item $TempFolder -Recurse -Confirm:$false -Force
+	}
+
+	It "should find files with specific tokens inside for both user modified Excel file and untouched Sharegate exported file in the folder" {
+
+			# Create a copy of the files
+			$TempFolder = New-Item -ItemType 'Directory' -Path $here -Name "Temp" -Force
+			$CopiedItemSharegate = Copy-Item -Path $ExcelValidFilePath -Destination $TempFolder -PassThru -Force
+			$CopiedItemUserModified = Copy-Item -Path $ExcelValidFilePathUserModified -Destination $TempFolder -PassThru -Force
+
+			$result = Find-DSPExcelFiles -Folder $TempFolder.FullName -Columns "Column1","Column2","User" -Patterns "dev\\danb","Value"
+			
+			$result.Count | Should Be 2
+			$result[0]."Matching lines count" | Should Be 3
+			$result[1]."Matching lines count" | Should Be 3
+
+			# Test teardown
+			Remove-Item $TempFolder -Recurse -Confirm:$false -Force
+	}
+
 }
