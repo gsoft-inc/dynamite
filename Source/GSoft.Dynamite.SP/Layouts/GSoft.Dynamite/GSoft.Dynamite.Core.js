eff6e1d17ef7165759f69250eae72bd946587028
Alexandre Massé <alexandre.masse@gsoft-group.com>
2015-06-05 14:04:19 -0400

    [JSHint] Fix "redefinition of console" error. I hope I kept the intended logic!

--- Source/GSoft.Dynamite.SP/Layouts/GSoft.Dynamite/GSoft.Dynamite.Core.js
+++ Source/GSoft.Dynamite.SP/Layouts/GSoft.Dynamite/GSoft.Dynamite.Core.js
@@ -31,15 +31,13 @@ moment.locale('en', {
 });
 
 // IE9 fix
-if (!window.console) {
-    var console = {
-        log: function () { },
-        warn: function () { },
-        error: function () { },
-        time: function () { },
-        timeEnd: function () { }
-    }
-}
+window.console = window.console || {
+    log: function() {},
+    warn: function() {},
+    error: function() {},
+    time: function() {},
+    timeEnd: function() {}
+};
 
 // GSoft namespace root
 window.GSoft = window.GSoft || {};
